51d139a3a221ed2f6ea0d8bedd00f3b58fea81d1
TrangNgocHuyen <129882308+TrangNgocHuyen@users.noreply.github.com>
2024-03-16 14:32:26 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
+    public static void main(String[] args) throws InterruptedException {
+        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
+
         SumThread sumThread = new SumThread(numbers);
         MaxThread maxThread = new MaxThread(numbers);
 
@@ -11,7 +12,7 @@ public class Main {
 
         sumThreadObj.start();
         maxThreadObj.start();
-        
+
         sumThreadObj.join();
         maxThreadObj.join();
 
@@ -19,6 +20,7 @@ public class Main {
         System.out.println("Tổng của các phần tử trong mảng là: " + sumThread.getSum());
     }
 }
+
 class SumThread implements Runnable {
     private int[] numbers;
     private int sum;
